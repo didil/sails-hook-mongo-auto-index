2048e4fc0a4b83b7c03bedc1e3ec8a58103265c5
didil <haritahadil@yahoo.fr>
2015-10-29 12:26:03 +0000

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+# sails-hook-mongo-auto-index
+
+In production, sails.js does not create mongo indexes/unique indexes defined on the models, so manual index creation
+ is necessary (https://github.com/balderdashy/sails-mongo/issues/290).  
+  
+This hook tries to solve this issue by using mongo's ensureIndex function to create the indexes in all environments.  
+
+The indexes are automatically created based on the attributes found on your waterline models : 
+
+````json
+    name: {
+      type: 'string',
+      index: true
+    }
+````
+Or 
+````json
+    name: {
+      type: 'string',
+      unique: true
+    }
+````
---
+++ index.js
@@ -0,0 +1,89 @@
+var _ = require('lodash');
+var async = require('async');
+
+module.exports = function mongoAutoIndexHook(sails) {
+
+  function getIndexesFromModels() {
+    var indexes = [];
+
+    _.forIn(sails.models, function (model,modelKey) {
+      if(sails.config.connections[model.connection].adapter !== 'sails-mong'){
+        sails.log.verbose('sails-hook-mongo-auto-index: ' + 'skipping model ' + modelKey + ', not a sails-mongo model');
+        return;
+      }
+
+      _.forIn(model.attributes, function (attribute, attributeKey) {
+        if (attribute.primaryKey) {
+          return; // Sails autocreates primary key indexes
+        }
+
+        if (attribute.unique) {
+          indexes.push(
+            {
+              model: model,
+              attributeKey: attributeKey,
+              unique: true
+            }
+          );
+        }
+
+        if (attribute.index === true) {
+          indexes.push(
+            {
+              model: model,
+              attributeKey: attributeKey,
+              unique: false
+            }
+          );
+        }
+
+      });
+    });
+
+    return indexes;
+  }
+
+  function createIndexes(indexes, cb) {
+    var cbCalled = false; // control variable to call the hook callback only once
+    async.each(indexes, createIndex, function (err) {
+      if (err) {
+        if(!cbCalled){
+          cbCalled = true;
+          cb(err);
+        }
+        return;
+      }
+
+      if(!cbCalled){
+        cbCalled = true;
+        cb();
+      }
+    });
+  }
+
+  function createIndex(index, cb) {
+    var model = index.model,
+      attributeKey = index.attributeKey,
+      unique = index.unique;
+
+    model.native(function (err, collection) {
+      collection.ensureIndex(attributeKey, {unique: unique}, function (err, result) {
+        if (err) {
+          return cb(err);
+        }
+
+        cb();
+      });
+    });
+  }
+
+  return {
+    initialize: function (cb) {
+      sails.on('hook:orm:loaded', function () {
+        var indexes = getIndexesFromModels();
+
+        createIndexes(indexes, cb);
+      });
+    }
+  };
+};
